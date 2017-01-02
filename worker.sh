666cddae09f6e8ddee209d18a08f056c95edaab3
Rub21 <ruben@mapbox.com>
2017-01-02 16:03:32 -0500

    minor issue

--- worker.sh
+++ worker.sh
@@ -4,7 +4,7 @@ mkdir $DATA
 # chmod 777 -R $DATA
 wget https://s3.amazonaws.com/mapbox/osm-qa-tiles/latest.country/united_states_of_america.mbtiles.gz -O $DATA/united_states_of_america.mbtiles.gz
 gunzip  $DATA/united_states_of_america.mbtiles.gz
-# wget https://s3.amazonaws.com/mapbox/tile-reduce-watchbot/mbtiles/tiger2015.mbtiles -O  $DATA/tiger2016.mbtiles
+# wget https://s3.amazonaws.com/mapbox/tile-reduce-watchbot/mbtiles/tiger2016.mbtiles -O  $DATA/tiger2016.mbtiles
 wget https://s3.amazonaws.com/mapbox/tile-reduce-watchbot/mbtiles/tiger2015.mbtiles -O  $DATA/tiger2015.mbtiles
 node index.js $1 > $DATA/bbox.txt
 filename="$DATA/bbox.txt"
@@ -14,7 +14,7 @@ do
     osmlint tigerdelta --bbox="$next" --zoom=12 $DATA/united_states_of_america.mbtiles $DATA/tiger2015.mbtiles > $DATA/tiger-delta-$next.json
 done
 cat  $DATA/tiger-delta-*.json > $DATA/tiger.json
-rm tiger-delta-*.json
+rm $DATA/tiger-delta-*.json
 osmlint-filter -g $DATA/tiger.json > $DATA/tiger.geojson
 numFeatures=$(grep -o "Feature" $DATA/tiger.geojson | wc -l)
 echo "num Features : $numFeatures"
